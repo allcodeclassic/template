a8055838c8b0505665506892dca5b4eead96f691
hmtrung2305 <61732256+allcodeclassic@users.noreply.github.com>
2022-03-02 14:48:57 +0700

    Update classic.cpp

--- classic.cpp
+++ classic.cpp
@@ -3,6 +3,64 @@
 #include <bits/stdc++.h>
 
 namespace classic {
+	long long Dijkstra(int start, int ends, std::vector<std::vector<std::pair<long long, int>>> &adjacent) {
+		// Dijkstra's algorithm
+		// shortest path weight from start to ends
+		// time complexity O(vertex + egde * log(vertex))
+		int size = (int)adjacent.size();
+		std::vector<long long> dist(size, 1e15);
+		std::priority_queue<std::pair<long long, int>, std::vector<std::pair<long long, int>>, std::greater<std::pair<long long, int>>> min_heap;
+		min_heap.emplace(0, start);
+		dist[start] = 0;
+		while (!min_heap.empty()) {
+			std::pair<long long, int> current = min_heap.top(); 
+			long long dist_current = current.first;
+			int vertex_current = current.second;
+			min_heap.pop();
+			if (dist_current > dist[vertex_current]) {
+				continue;
+			}
+			for (std::pair<long long, int> next : adjacent[vertex_current]) {
+				long long dist_current_to_next = next.first;
+				int vertex_next = next.second;
+				if (dist[vertex_next] > dist[vertex_current] + dist_current_to_next) {
+					dist[vertex_next] = dist[vertex_current] + dist_current_to_next;
+					min_heap.emplace(dist[vertex_next], vertex_next);
+				}
+			}
+		}
+		return dist[ends];
+	}
+
+	std::vector<long long> Dijkstra(int start, std::vector<std::vector<std::pair<long long, int>>> &adjacent) {
+		// Dijkstra's algorithm
+		// shortest path weight from start to all vertices
+		// time complexity O(vertex + egde * log(vertex))
+		int size = (int)adjacent.size();
+		std::vector<long long> dist(size, 1e15);
+		std::priority_queue<std::pair<long long, int>, std::vector<std::pair<long long, int>>, std::greater<std::pair<long long, int>>> min_heap;
+		min_heap.emplace(0, start);
+		dist[start] = 0;
+		while (!min_heap.empty()) {
+			std::pair<long long, int> current = min_heap.top(); 
+			min_heap.pop();
+			long long dist_current = current.first;
+			int vertex_current = current.second;
+			if (dist_current > dist[vertex_current]) {
+				continue;
+			}
+			for (std::pair<long long, int> next : adjacent[vertex_current]) {
+				long long dist_current_to_next = next.first;
+				int vertex_next = next.second;
+				if (dist[vertex_next] > dist[vertex_current] + dist_current_to_next) {
+					dist[vertex_next] = dist[vertex_current] + dist_current_to_next;
+					min_heap.emplace(dist[vertex_next], vertex_next);
+				}
+			}
+		}
+		return dist;
+	}
+	
 	struct Edmonds_Karp {
 		int size;
 		std::vector<std::vector<int>> adjacent;
